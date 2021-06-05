91550df9d4ac322a8ca157646d3b9a9c5fb2e6d0
Aravind <kraravind1993@gmail.com>
2021-06-05 20:36:01 +0530

    Team Page Styling

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -1,6 +1,5 @@
 import { React, useEffect, useState } from "react";
 import { MatchDetailCard } from "../components/MatchDetailCard";
-import { MatchSmallCard } from "../components/MatchSmallCard";
 import {useParams} from 'react-router-dom';
 
 export const MatchPage = () => {
--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -2,6 +2,8 @@ import { React, useEffect, useState } from "react";
 import { MatchDetailCard } from "../components/MatchDetailCard";
 import { MatchSmallCard } from "../components/MatchSmallCard";
 import {useParams} from 'react-router-dom';
+import './TeamPage.scss';
+import { PieChart } from 'react-minimal-pie-chart';
 
 export const TeamPage = () => {
   
@@ -24,10 +26,26 @@ export const TeamPage = () => {
     }
     return (
         <div className="TeamPage">
-        <h1>{team.teamName}</h1>
-
-        <MatchDetailCard teamName={team.teamName} match={team.matches[0]}/>
-        {team.matches.slice(1).map(match => <MatchSmallCard teamName={team.teamName} match={match}/>)}  
+            <div className="team-name-section">
+                <h1 className="team-name">{team.teamName}</h1>
+            </div>
+            <div className="wins-loss-section">
+                Wins / Losses
+                <PieChart
+                    data={[
+                        { title: 'Losses', value: team.totalMatches - team.totalWins, color: 'maroon' },
+                        { title: 'Wins', value: team.totalWins, color: 'green' },
+                    ]}
+                    />
+            </div>
+            <div className="match-detail-section">
+                <h3>Latest Matches</h3>
+                <MatchDetailCard teamName={team.teamName} match={team.matches[0]}/>
+            </div>
+            {team.matches.slice(1).map(match => <MatchSmallCard teamName={team.teamName} match={match}/>)}  
+            <div className="more-link">
+                <a href="#">More {">>"} </a>
+            </div>
         </div>
     );
 }
